46ed2c3ef88bc8b1178ee9e943bb05c939fc78e5
Komakivan <91111276+Komakivan@users.noreply.github.com>
2026-05-04 03:00:40 +0000

    Connected Nodejs api

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -2,32 +2,31 @@ import React, { useState, useEffect } from 'react'
 import Card from './Card'
 import Button from './Button'
 import Search from './Search'
+import { BASE_URL } from '../config'
 
-const CardList = ({ data }) => {
+export const CardList = ({}) => {
   // define the limit state variable and set it to 10
   const limit = 10;
 
   // Define the offset state variable and set it to 0
   const [offset, setOffset] = useState(0);
   // Define the products state variable and set it to the default dataset
-  const [products, setProducts] = useState(data);
+  // Define the state object for product data
+  const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    setProducts(data.slice(offset, offset + limit));
-  }, [offset, limit, data])
-
-  const filterTags = (tagQuery) => {
-    const filtered = data.filter(product => {
-      if (!tagQuery) {
-        return product
-      }
-
-      return product.tags.find(({title}) => title === tagQuery)
-    })
-
-    setOffset(0)
-    setProducts(filtered)
+  // Update fetch projects to include the limit and offset parameters
+  const fetchProducts = () => {
+    fetch(`${BASE_URL}/products?offset=${offset}&limit=${limit}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
   }
+  
+  // Update the `useEffect` to monitor the `offset` state variable
+  useEffect(() => {
+   fetchProducts();
+  }, [offset]);
 
 
   return (
